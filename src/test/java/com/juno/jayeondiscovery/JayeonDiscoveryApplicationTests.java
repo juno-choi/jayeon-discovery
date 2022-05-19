2f5090934a7a99aaa00d91cf0d9ddcd2366c9095
juno-choi <ililil9482@naver.com>
2022-05-19 21:15:06 +0900

    FEAT : docker-compose.yml 파일 추가

--- src/test/java/com/juno/jayeondiscovery/JayeonDiscoveryApplicationTests.java
+++ src/test/java/com/juno/jayeondiscovery/JayeonDiscoveryApplicationTests.java
@@ -3,7 +3,7 @@ package com.juno.jayeondiscovery;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class JayeonDiscoveryApplicationTests {
 
     @Test
